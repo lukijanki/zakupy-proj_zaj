d81e2a359b0f8a987065f95e487f8b9fd77e8bf2
lukijanki <121889156+lukijanki@users.noreply.github.com>
2024-06-10 20:53:30 +0200

    Update add.php
    update kodu

--- add.php
+++ add.php
@@ -1,65 +1,64 @@
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Add List test</title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-        <link rel="stylesheet" href="css/style.css">
-    </head>
-    <body>
-        <div class="container mt-5">
-            <h1>Add Grocery List</h1>
-            <form action="add.php" method="POST">
-                <div class="form-group">
-                    <label>Item name</label>
-                    <input type="text" class="form-control" placeholder="Item name" name="iname"/>
-                </div>
-                <div class="form-group">
-                    <label>Item quantity</label>
-                    <input type="text" class="form-control" placeholder="Item quantity"  name="iqty"/>
-                </div>
-                <div class="form-group">
-                    <label>Item status</label>
-                    <select class="form-control" name="istatus">
-                        <option value="0" >PENDING</option>
-                        <option value="1">BOUGHT</option>
-                        <option value="2">NOT AVAILABLE</option>
-                    </select>
-                </div>
-                <div class="form-group">
-                    <label>Date</label>
-                    <input type="date" class="form-control" placeholder="Date" name="idate">
-                </div>
-                <div class="form-group">
-                    <input type="submit" value="Add" class="btn btn-danger" name="btn">
-                </div>
-            </form>
-        </div>
-		<?php
-           if(isset($_POST["btn"]))
-           {
-	           include("connect.php");
-               $item_name=$_POST['iname'];
-               $item_qty=$_POST['iqty'];
-               $item_status=$_POST['istatus'];
-               $date=$_POST['idate'];
-    
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Add Grocery Item</title> 
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="container mt-5">
+        <h1>Add Grocery Item</h1>
+        <form action="" method="POST"> 
+            <div class="form-group">
+                <label>Item Name:</label>
+                <input type="text" class="form-control" placeholder="Item name" name="iname" required> 
+            </div>
+            <div class="form-group">
+                <label>Item Quantity:</label>
+                <input type="number" class="form-control" placeholder="Item quantity" name="iqty" required> 
+            </div>
+            <div class="form-group">
+                <label>Item Status:</label>
+                <select class="form-control" name="istatus">
+                    <option value="0">PENDING</option>
+                    <option value="1">BOUGHT</option>
+                    <option value="2">NOT AVAILABLE</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label>Date:</label>
+                <input type="date" class="form-control" name="idate" required> 
+            </div>
+            <div class="form-group">
+                <input type="submit" value="Add" class="btn btn-danger" name="btn">
+            </div>
+        </form>
+    </div>
 
-               $q="insert into grocerytb(Item_name,Item_Quantity,Item_status,Date)values('$item_name',$item_qty,'$item_status','$date')";
+    <?php
+    if(isset($_POST["btn"])) {
+        include("connect.php"); 
 
-               mysqli_query($con,$q);
-               header("location:index.php");
+        // Pobranie danych z formularza
+        $item_name = $_POST['iname'];
+        $item_qty = $_POST['iqty'];
+        $item_status = $_POST['istatus'];
+        $date = $_POST['idate'];
 
-	 
-            }
-			// if(!mysqli_query($con,$q))
-            // {
-	             // echo "Value Not Inserted";
-             // }
-             // else
-             // {
-               	// echo "Value Inserted";
-             // }
-         ?>
-		
-    </body> 
+        // Zapytanie SQL z prepared statements (ochrona przed SQL injection)
+        $stmt = $conn->prepare("INSERT INTO grocerytb (Item_name, Item_Quantity, Item_status, Date) VALUES (?, ?, ?, ?)");
+        $stmt->bind_param("ssss", $item_name, $item_qty, $item_status, $date);
+
+        if ($stmt->execute()) {
+            header("Location: index.php"); // Przekierowanie po dodaniu
+            exit(); // Ważne, aby zakończyć skrypt po przekierowaniu
+        } else {
+            echo "Error: " . $stmt->error; // Wyświetl komunikat o błędzie
+        }
+
+        $stmt->close();
+        $conn->close(); 
+    }
+    ?>
+</body>
 </html>
