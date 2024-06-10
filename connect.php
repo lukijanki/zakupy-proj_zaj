7c10277a060d635b1a496477ea70242d24088cf6
lukijanki <121889156+lukijanki@users.noreply.github.com>
2024-06-10 20:08:53 +0200

    Update connect.php
    database connection

--- connect.php
+++ connect.php
@@ -1,7 +1,12 @@
 <?php
-		$con=mysqli_connect("localhost","root","","grocerydb");
-		if(!$con)
-		{
-		die("cannot connect to server");
-		}	
+$servername = "zakupyproj-server.mysql.database.azure.com";
+$username = "aatinfwcrt";
+$password = "xl2mPu$6bFyOIp3g";
+$db_name = "zakupyproj-database";
+
+$conn = new mysqli($servername, $username, $password, $db_name, 3306);
+
+if($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 ?>
