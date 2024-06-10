0c8877b4e385c5c6a3900a99bb7833903c9f8c50
lukijanki <121889156+lukijanki@users.noreply.github.com>
2024-06-10 20:25:39 +0200

    Update index.php
    test

--- index.php
+++ index.php
@@ -1,18 +1,4 @@
-<?php
-    include("connect.php");
 
-    if (isset($_POST['btn']))
-    {
-      $date=$_POST['idate'];
-      $q="select * from grocerytb where Date='$date'";
-      $query=mysqli_query($con,$q);
-    } 
-	else 
-	{
-      $q= "select * from grocerytb";
-      $query=mysqli_query($con,$q);
-    }
-?>
 
 <html>
     <head>
@@ -47,39 +33,7 @@
             <!-- Grocery Cards -->
             <div class="row mt-4">
                 
-             <?php
-                  while ($qq=mysqli_fetch_array($query)) 
-                  {
-                  
-             ?>
 
-                <div class="col-lg-4">
-                    <div class="card">
-                        <div class="card-body">
-                          <h5 class="card-title"><?php echo $qq['Item_name']; ?></h5>
-                          <h6 class="card-subtitle mb-2 text-muted"><?php echo $qq['Item_Quantity']; ?></h6>
-                          <?php
-                          if($qq['Item_status'] == 0) {
-                          ?>
-                            <p class="text-info">PENDING</p>
-                          <?php
-                          } else if($qq['Item_status'] == 1) {
-                          ?>
-                            <p class="text-success">BOUGHT</p>
-                          <?php } else { ?> 
-                            <p class="text-danger">NOT AVAILABLE</p>
-                          <?php } ?>
-                          <a href="delete.php?id=<?php echo $qq['Id']; ?>" class="card-link">Delete</a>
-    					            <a href="update.php?id=<?php echo $qq['Id']; ?>" class="card-link">Update</a>
-                        </div>
-
-                      </div><br>
-                </div>
-                <?php
-                  
-
-                  }
-                ?>
                 
             </div>
         </div>
