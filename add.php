9aa7036bc268c37f841e90cb9d44019336623e4b
lukijanki <121889156+lukijanki@users.noreply.github.com>
2024-06-10 20:32:04 +0200

    Update add.php

--- add.php
+++ add.php
@@ -1,7 +1,7 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Add List</title>
+        <title>Add List test</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
         <link rel="stylesheet" href="css/style.css">
     </head>
