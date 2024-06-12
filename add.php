414bcdce550f4340ac54ad9570d5d8874f2c69e8
Karolina <131374467+Kxrolcix8@users.noreply.github.com>
2024-06-12 10:24:26 +0200

    Update add.php
    upgrade

--- add.php
+++ add.php
@@ -1,38 +1,46 @@
-<html>
+<!DOCTYPE html>
+<html lang="pl">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <title>Dodaj produkt do magazynu</title> 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
+    
 </head>
 <body>
     <div class="container mt-5">
-        <h1>Dodaj produkt do magazynu</h1>
-        <form action="" method="POST"> 
-            <div class="form-group">
-                <label>Nazwa produktu:</label>
-                <input type="text" class="form-control" placeholder="Item name" name="iname" required> 
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="form-container">
+                    <h1 class="mb-4 text-center">Dodaj produkt do magazynu</h1>
+                    <form action="" method="POST"> 
+                        <div class="form-group">
+                            <label for="iname">Nazwa produktu:</label>
+                            <input type="text" class="form-control" id="iname" placeholder="Nazwa produktu" name="iname" required> 
+                        </div>
+                        <div class="form-group">
+                            <label for="iqty">Ilość:</label>
+                            <input type="number" class="form-control" id="iqty" placeholder="Ilość" name="iqty" required> 
+                        </div>
+                        <div class="form-group">
+                            <label for="istatus">Status:</label>
+                            <select class="form-control" id="istatus" name="istatus">
+                                <option value="0">Oczekuje na dostawę</option>
+                                <option value="1">Dostępny</option>
+                                <option value="2">Niedostępny</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="idate">Data:</label>
+                            <input type="date" class="form-control" id="idate" name="idate" required> 
+                        </div>
+                        <div class="form-group text-center">
+                            <input type="submit" value="Dodaj" class="btn btn-primary btn-lg" name="btn">
+                        </div>
+                    </form>
+                </div>
             </div>
-            <div class="form-group">
-                <label>Ilość:</label>
-                <input type="number" class="form-control" placeholder="Item quantity" name="iqty" required> 
-            </div>
-            <div class="form-group">
-                <label>Status:</label>
-                <select class="form-control" name="istatus">
-                    <option value="0">Oczekuje na dostawę</option>
-                    <option value="1">Dostępny</option>
-                    <option value="2">Niedostępny</option>
-                </select>
-            </div>
-            <div class="form-group">
-                <label>Date:</label>
-                <input type="date" class="form-control" name="idate" required> 
-            </div>
-            <div class="form-group">
-                <input type="submit" value="Add" class="btn btn-danger" name="btn">
-            </div>
-        </form>
+        </div>
     </div>
 
     <?php
@@ -45,7 +53,6 @@
         $item_status = $_POST['istatus'];
         $date = $_POST['idate'];
 
-     
         $stmt = $conn->prepare("INSERT INTO grocerytb (Item_name, Item_Quantity, Item_status, Date) VALUES (?, ?, ?, ?)");
         $stmt->bind_param("ssss", $item_name, $item_qty, $item_status, $date);
         //Przekierowanie do index.php
@@ -60,5 +67,9 @@
         $conn->close(); 
     }
     ?>
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script> 
 </body>
 </html>
